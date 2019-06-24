0e8f4243ca2b0965d05c02a0f934085ccdf7dd52
Pavel Druzhinin <druzini@gmail.com>
2019-06-24 14:38:23 +0300

    Последняя рабочая версия

--- ConsoleMonitor/Program.cs
+++ ConsoleMonitor/Program.cs
@@ -3,25 +3,17 @@ using System.Diagnostics;
 using System.Threading;
 using System.IO;
 using System.Text;
+
 namespace ConsoleMonitor
 {
     class Program
     {
        static void Main(string[] args)
         {
-            if (args.Length < 3)
+            // Если пришло достаточное количество аргуметов
+            if (args.Length == 3 || args.Length == 4)
             {
-                Console.WriteLine("Необходимо вести 3 аргумента:");
-                Console.WriteLine("1: Имя процесса, например \"notepad\"");
-                Console.WriteLine("2: Время жизни процесса в минутах");
-                Console.WriteLine("3: Время опроса в минутах");
-                Console.WriteLine("4: Можно задать имя файла лога (Опционально)");
-                Console.WriteLine("Пример1: ConsoleMonitor notepad 5 1");
-                Console.WriteLine("Пример2: ConsoleMonitor notepad 5 1 CustomLogFile.txt");
-            }
-            else if (args.Length == 3 || args.Length == 4)
-            {
-
+                // Пытаемся считать все параметры
                 String nameProcess = args[0].ToString();
                 int lifeTimeProcess = 0;
                 int touchTimeProcess = 0;
@@ -30,7 +22,7 @@ namespace ConsoleMonitor
                 {
                     logFileName = args[3];
                 }
-
+                // Попробуем сконвертировать параметры времени в Int если не вышло, то выдадим ошибку.
                 try
                 {
                     lifeTimeProcess = Convert.ToInt32(args[1]);
@@ -40,34 +32,40 @@ namespace ConsoleMonitor
                 {
                     Console.WriteLine("Ошибка в параметрах времени: " + e.Message.ToString());
                 }
-
-                Console.WriteLine("Отслеживаем процесс: " + nameProcess + " на время выполнения " + lifeTimeProcess + " мин. каждые " + touchTimeProcess + " мин.");
-
+                // Если ввели все параметры верно, то идём отслеживать процессы.
                 if (lifeTimeProcess > 0 & touchTimeProcess > 0)
                 {
+                    Console.WriteLine("Отслеживаем процесс: " + nameProcess + " на время выполнения " + lifeTimeProcess + " мин. каждые " + touchTimeProcess + " мин.");
+                    Console.WriteLine("Для остановки мониторинга нажмите Spacebar, Enter или x");
                     var LongProgramTime = DateTime.Now;
                     WriteLog(DateTime.Now + " Начало отслеживания " + nameProcess, logFileName);
                     ConsoleKeyInfo cki;
+                    // Заводи бесконечный цикл, в которм будем отследивать нажатие клавиши для выхода и процессы.
                     while (true)
                     {
+                        // Проверяем что прошло необходимое время для опроса заданное в третьем аргументе
                         if(LongProgramTime < DateTime.Now)
                         {
+                            // Получаем все процессы с заданным именем.
                             var processes = GetProcess(nameProcess);
                             if (processes.Length > 0)
                             {
                                 foreach (Process prc in processes)
                                 {
-                                    // Console.WriteLine("Процесс: " + prc.ProcessName + " ID " + prc.Id + " зпущен в " + prc.StartTime + " сейчас " + DateTime.Now);
+                                    // Если превышено время отслеживаемого процесса мы его убиваем.
                                     if (prc.StartTime < DateTime.Now.AddMinutes(-lifeTimeProcess)) {
-                                        KillProcessById(prc.Id);
-                                        WriteLog(DateTime.Now + " Процесс " + nameProcess + " был остановлен", logFileName);
+                                        var processId = prc.Id;
+                                        KillProcessById(processId);
+                                        WriteLog(DateTime.Now + " Процесс " + nameProcess + " ID (" + processId + ") был остановлен", logFileName);
                                     }
                                 }
                             }
                             else
                             {
-                                WriteLog(DateTime.Now + " Процесс " + nameProcess + " пока не запущен", logFileName);
+                                // Можно писать в лог если процесс пока не запущен.
+                                // WriteLog(DateTime.Now + " Процесс " + nameProcess + " пока не запущен", logFileName);
                             }
+                            // Добавляем количество минут для следующего опроса.
                             LongProgramTime = DateTime.Now.AddMinutes(touchTimeProcess);
                         }
                         Thread.Sleep(1000);
@@ -85,18 +83,31 @@ namespace ConsoleMonitor
                 }
                 else
                 {
-                    Console.WriteLine("Не все параметры введены корректно, или нет процесса с таким именем.");
+                    // Есои не все аргументы введены или их слишком много.
+                    Console.WriteLine("Не все параметры введены корректно.");
+                    Console.WriteLine("Необходимо вести 3 аргумента:");
+                    Console.WriteLine("1: Имя процесса, например \"notepad\"");
+                    Console.WriteLine("2: Время жизни процесса в минутах");
+                    Console.WriteLine("3: Время опроса в минутах");
+                    Console.WriteLine("4: Можно задать имя файла лога (Опционально)");
+                    Console.WriteLine("Пример1: ConsoleMonitor notepad 5 1");
+                    Console.WriteLine("Пример2: ConsoleMonitor notepad 5 1 CustomLogFile.txt");
                 }
             }
         }
-
+        /**
+         * @Args int idToKill - Убиваем процесс по ID
+         */
         public static void KillProcessById(int idToKill)
         {
-            foreach (Process process in Process.GetProcesses())
-                if (process.Id == idToKill)
-                    process.Kill();
+            Process process = Process.GetProcessById(idToKill);
+            process.Kill();
         }
 
+        /**
+         * @Args String nameProcess
+         * @Output Array of Process where name is nameProcess
+         */
         protected static Process[] GetProcess(String nameProcess)
         {
             Process[] prcesses = Process.GetProcessesByName(nameProcess);
@@ -107,7 +118,7 @@ namespace ConsoleMonitor
         {
             try
             {
-                StreamWriter writer = new System.IO.StreamWriter(fileName);
+                StreamWriter writer = new System.IO.StreamWriter(fileName,true);
                 writer.WriteLine(text);
                 writer.Close();
                 return true;
